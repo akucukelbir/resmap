72f7a6a4f147b14242b46173b74516e096b67c0e
Alp Kucukelbir <alp@server.fake>
2013-04-05 17:16:53 -0400

    prewhitening implemented

--- localResolution3D.py
+++ localResolution3D.py
@@ -12,6 +12,7 @@ from scipy.ndimage import filters
 # from tkFileDialog import askopenfilename
 
 # Modules found in python files in root folder
+from sphericalProfile import *
 from localreshelpers import *
 from blocks import *
 from mrc import *
@@ -27,22 +28,68 @@ if __name__ == '__main__':
 	tBegin = time()
 
 	# User defined parameters
-	inputFileName = 'EMD-2277.map'
+
+	# inputFileName = 'EMD-2275.map'
+	# vxSize = 1.77	# voxel size (in Angstroms)
+	# Mmax   = 7
+
+	# inputFileName = 'EMD-2277.map'
+	# vxSize = 1.77	# voxel size (in Angstroms)
+	# Mmax   = 7
+
+	inputFileName = 'EMD-2277_down.map'
+	vxSize = (8.0/6.0)*1.77	# voxel size (in Angstroms)
+	Mmax   = 8
+
+	# inputFileName = 'EMD-2238.map'
+	# vxSize = 2.45	# voxel size (in Angstroms)	
+	# Mmax   = 10
+
+	# inputFileName = 'EMD-2238_down.map'
+	# vxSize = (8.0/6.0)*2.45	# voxel size (in Angstroms)	
+	# Mmax   = 10
+
+
+	# inputFileName = 'EMD-5562.map'
+	# vxSize = 2.71	# voxel size (in Angstroms)	
+	# Mmax   = 14
+
+	# inputFileName = 'EMD-5562_down.map'
+	# vxSize = (8.0/5.0)*2.71	# voxel size (in Angstroms)	
+	# Mmax   = 14
+
+	# inputFileName = 'EMD-2012.map'
+	# vxSize = 1.81	# voxel size (in Angstroms)	
+	# Mmax   = 10
+
+	# inputFileName = 'EMD-2012_down.map'
+	# vxSize = (8.0/5.0)*1.81	# voxel size (in Angstroms)	
+	# Mmax   = 10
+
+	# inputFileName = 'class4_half1.map'
+	# vxSize = 1.77	# voxel size (in Angstroms)	
+	# Mmax   = 8
+
+	# inputFileName = 'class4_half1_pw.map'
+	# vxSize = 1.77	# voxel size (in Angstroms)	
+	# Mmax   = 8
+
+	# inputFileName = 'threed_12a_pw1.map'
+	# vxSize = 3.09	# voxel size (in Angstroms)	
+	# Mmax = 15
+
+	# inputFileName = 'nramm_ab_pw1.map'
+	# vxSize = 3.09	# voxel size (in Angstroms)	
+	# Mmax = 15
+
 	(fname,ext)   = os.path.splitext(inputFileName)
 
-	vxSize = 1.77	# voxel size (in Angstroms)
-
-	# Mbegin = 4.5	# query resolution (in Angstroms)
-	# Mend   = 7.5
-	# Mstep  = 0.5
-
-	Mbegin = round((2.5*vxSize)/0.5)*0.5	# round to the nearest 0.5
+	# Start from M = 2.5*vxSize, take steps of Mstep up
+	Mbegin = round((2.1*vxSize)/0.5)*0.5	# round to the nearest 0.5
 	Mstep  = 0.5
 	M      = Mbegin
 
 	pValue = 0.05	# generally between (0, 0.05]
-
-	# Marray = np.arange(Mbegin, Mend+Mstep, Mstep)
 
 	# minRes = 2.5*vxSize
 	# if Mbegin < minRes:
@@ -50,42 +97,98 @@ if __name__ == '__main__':
 	# 	raise Exception('Take care of this')
 
 	# Load files from MRC file
+	print '\n\n= Loading Volume'
+	tStart    = time()
 	data = mrc_image(inputFileName)
 	data.read()
 	data = data.image_data
 	n 	 = data.shape[0]
 	N 	 = int(n)
+	m, s      = divmod(time() - tStart, 60)
+	print "  :: Time elapsed: %d minutes and %.2f seconds" % (m, s)
 
+	print '\n= Computing Mask Separating Particle from Background'
+	tStart    = time()
+	# We assume the particle is at the center of the volume
 	# Create spherical mask
 	[x,y,z] = np.mgrid[ -n/2:n/2:complex(0,n),
 						-n/2:n/2:complex(0,n),
 						-n/2:n/2:complex(0,n) ]
-	R       = np.sqrt(x**2 + y**2 + z**2)
+	R       = np.array(np.sqrt(x**2 + y**2 + z**2),dtype='float32')
 	Rinside = R < n/2
 	del (x,y,z)	
+
+	# Compute mask separating the particle from background
+	dataBlurred  = filters.gaussian_filter(data, float(n*0.02))	# kernel size 2% of n
+	dataMask     = dataBlurred > np.max(dataBlurred)*1e-1
+	mask         = np.bitwise_and(dataMask, R < n/2 - 2)
+	# maskBG       = np.bitwise_and(np.logical_not(dataMask), R < n/2 - 2)
+	# maskBGsmooth = filters.gaussian_filter(maskBG, float(n*0.05))	# kernel size 5% of n
+	oldSumOfMask = np.sum(mask)
+	del dataMask	
+
+	m, s      = divmod(time() - tStart, 60)
+	print "  :: Time elapsed: %d minutes and %.2f seconds" % (m, s)
+
+	print '\n= Calculating Spherically Averaged Power Spectrum of Volume'
+	tStart    = time()
+	dataF     = np.fft.fftshift(np.fft.fftn(data))
+	dataFabs  = np.array(np.abs(dataF), dtype='float32')
+	dataFabs  = dataFabs-np.min(dataFabs)
+	dataFabs  = dataFabs/np.max(dataFabs)
+	dataSpect = sphericalAverage(dataFabs) + 1e-10
+	m, s      = divmod(time() - tStart, 60)
+	print "  :: Time elapsed: %d minutes and %.2f seconds" % (m, s)
+
+	print '\n= Pre-whitening Volume'
+	tStart    = time()
+	xpoly = np.array(range(1,dataSpect.size + 1))
+	ypoly = np.log(dataSpect)
+	wpoly = np.array(np.bitwise_and(xpoly>n/4, xpoly<n/2-n/20), dtype='float32')
+	pcoef = np.polynomial.polynomial.polyfit(xpoly, ypoly, 2, w=wpoly)
+	peval = np.polynomial.polynomial.polyval(xpoly,pcoef)
+
+	R[R>n/2] = n/2
+	Reval    = np.polynomial.polynomial.polyval(R,-1*pcoef)
+	pWfilter = np.exp(Reval)
+
+	dataPW = np.real(np.fft.ifftn(np.fft.ifftshift(pWfilter*dataF)))
+
+	dataPWF     = np.fft.fftshift(np.fft.fftn(dataPW))
+	dataPWFabs  = np.array(np.abs(dataPWF), dtype='float32')
+	dataPWFabs  = dataPWFabs-np.min(dataPWFabs)
+	dataPWFabs  = dataPWFabs/np.max(dataPWFabs)
+	dataPWSpect = sphericalAverage(dataPWFabs) + 1e-10
+	
+	m, s      = divmod(time() - tStart, 60)
+	print "  :: Time elapsed: %d minutes and %.2f seconds" % (m, s)
+
+	plt.plot(xpoly,dataSpect**2,'b', 
+			 xpoly,np.exp(peval)**2,'r',
+			 xpoly,dataPWSpect**2,'m')
+	plt.yscale('log')
+	plt.show()
+
+	f, (ax1, ax2) = plt.subplots(1, 2)
+	ax1.imshow(data[int(n/2),:,:], cmap=plt.cm.gray, interpolation="nearest")
+	ax2.imshow(dataPW[int(n/2),:,:], cmap=plt.cm.gray, interpolation="nearest")
+	plt.show()
+
+	data = dataPW
+
+
 
 	resTOTAL = np.zeros_like(data)
 
 	moreToProcess = True
-	# for M in Marray:
 	while moreToProcess:
 
 		print '\n\n= Calculating Local Resolution for %.2f Angstroms\n' % M
 		# Compute window size and form steerable bases
-		# r       = np.ceil(0.5*M/vxSize)  	# number of pixels around center
-		# s       = (2.0*r*vxSize)/M      	# scaling factor to account for overshoot due to vxSize
-		# l       = np.pi*np.sqrt(2.0/5)		# lambda	
 		r       = np.ceil(0.5*M/vxSize)			# number of pixels around center
 		a       = (2*np.pi/M) * np.sqrt(2.0/5)	# scaling factor so that peak occurs at 1/M Angstroms
-		winSize = 2*r+1				 	
-
-		if M == Mbegin:
-			# Compute mask separating the particle from background
-			dataBlurred = filters.gaussian_filter(data, float(n/1e2))
-			dataMask    = dataBlurred > np.max(dataBlurred)*1e-1
-			mask        = np.bitwise_and(dataMask, R < n/2 - 2*winSize)
-			del dataMask
-			del R
+		winSize = 2*r+1		
+		print "winSize = %.2f" % winSize
 
 		# f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 		# ax1.imshow(data[:,:,int(3*n/9)], cmap=plt.cm.gray, interpolation="nearest")
@@ -101,9 +204,6 @@ if __name__ == '__main__':
 		# plt.show()
 
 		# Define range of x, y, z for steerable bases
-		# [x,y,z] = np.mgrid[	-s*l:s*l:complex(0,winSize),
-		# 					-s*l:s*l:complex(0,winSize),
-		# 					-s*l:s*l:complex(0,winSize) ]
 		[x,y,z] = a*np.mgrid[	-r*vxSize:r*vxSize:complex(0,winSize),
 								-r*vxSize:r*vxSize:complex(0,winSize),
 								-r*vxSize:r*vxSize:complex(0,winSize) ]		
@@ -136,6 +236,11 @@ if __name__ == '__main__':
 		Ac = np.zeros([numberOfPoints, 1])
 		Ac[:,0] = np.ones_like(kernel)
 
+		# debug
+		Ad = A[:,1:17]
+		Hd = np.dot(Ad, np.dot(np.linalg.pinv(np.dot(np.diag(kernelSqrt),Ad)), np.diag(kernelSqrt)))
+		LAMBDAd = W-np.dot(W,Hd)
+
 		# Invert weighted A matrix via SVD	
 		H = np.dot(A, np.dot(np.linalg.pinv(np.dot(np.diag(kernelSqrt),A)), np.diag(kernelSqrt)))
 
@@ -149,47 +254,50 @@ if __name__ == '__main__':
 		LAMBDAdiff = np.array(LAMBDAc-LAMBDA, dtype='float32')
 
 		## Estimate variance
-		if M == Mbegin:
-			print 'Estimating variance from non-overlapping blocks in background...',
-			tStart = time()
+	# if M == Mbegin:
+		print 'Estimating variance from non-overlapping blocks in background...',
+		tStart = time()
 
+		if M == Mbegin:
 			# Calculate mask of background voxels within Rinside sphere but outside of particle mask
 			maskBG = Rinside-mask
 
-			# Use numpy stride tricks to compute "view" into NON-overlapping
-			# blocks of 2*r+1. Does not allocate any extra memory
-			maskBGview = rolling_window(Rinside-mask, 
-							window=(winSize, winSize, winSize), 
-							asteps=(winSize, winSize, winSize))
+		# Use numpy stride tricks to compute "view" into NON-overlapping
+		# blocks of 2*r+1. Does not allocate any extra memory
+		maskBGview = rolling_window(maskBG, 
+						window=(winSize, winSize, winSize), 
+						asteps=(winSize, winSize, winSize))
 
-			dataBGview = rolling_window(data, 
-							window=(winSize, winSize, winSize), 
-							asteps=(winSize, winSize, winSize))
+		dataBGview = rolling_window(data, 
+						window=(winSize, winSize, winSize), 
+						asteps=(winSize, winSize, winSize))
 
-			# Find blocks within maskBG that are all 1s (i.e. only contain background voxels)
-			blockMaskBG = np.squeeze(np.apply_over_axes(np.all, maskBGview, [3,4,5]))
+		# Find blocks within maskBG that are all 1s (i.e. only contain background voxels)
+		blockMaskBG = np.squeeze(np.apply_over_axes(np.all, maskBGview, [3,4,5]))
 
-			# Get the i, j, k indices where the blocks are all 1s
-			blockMaskBGindex = np.array(np.where(blockMaskBG))
+		# Get the i, j, k indices where the blocks are all 1s
+		blockMaskBGindex = np.array(np.where(blockMaskBG))
 
-			dataBGcube = np.zeros(numberOfPoints,			dtype='float32')
-			WRSSBG     = np.zeros(blockMaskBGindex.shape[1],dtype='float32')
+		dataBGcube = np.zeros(numberOfPoints,			dtype='float32')
+		WRSSBG     = np.zeros(blockMaskBGindex.shape[1],dtype='float32')
 
-			# For each block, use 3D steerable model to estimate sigma^2
-			for idx in range(blockMaskBGindex.shape[1]):
-				i, j, k     = blockMaskBGindex[:,idx]
-				dataBGcube  = dataBGview[i,j,k,...].flatten()
-				WRSSBG[idx] = np.vdot(dataBGcube, np.dot(LAMBDA, dataBGcube))
-			WRSSBG = WRSSBG/np.trace(LAMBDA)
+		# For each block, use 3D steerable model to estimate sigma^2
+		for idx in range(blockMaskBGindex.shape[1]):
+			i, j, k     = blockMaskBGindex[:,idx]
+			dataBGcube  = dataBGview[i,j,k,...].flatten()
+			dataBGcube  = dataBGcube - np.mean(dataBGcube)
+			WRSSBG[idx] = np.vdot(dataBGcube, np.dot(LAMBDAd, dataBGcube))
+		WRSSBG = WRSSBG/np.trace(LAMBDAd)
 
-			# Estimate variance as mean of sigma^2's in each block
-			variance = np.mean(WRSSBG)
+		# Estimate variance as mean of sigma^2's in each block
+		variance = np.mean(WRSSBG)
 
-			print 'done.'
-			m, s = divmod(time() - tStart, 60)
-			print "  :: Time elapsed: %d minutes and %.2f seconds" % (m, s)
-			del maskBGview 
-			del dataBGview
+		print 'done.'
+		m, s = divmod(time() - tStart, 60)
+		print "  :: Time elapsed: %d minutes and %.2f seconds" % (m, s)
+		del maskBGview 
+		del dataBGview
+		print "variance = %.6f" % variance
 
 		## Compute Likelihood Ratio Statistic
 
@@ -294,16 +402,23 @@ if __name__ == '__main__':
 
 		# Update the mask to voxels that failed this level's likelihood test
 		mask = np.array(mask - res, dtype='bool')
-		print np.sum(mask)
-		if np.sum(mask)<n**2:
+		newSumOfMask = np.sum(mask)
+		print "oldSumOfMask = %d" % oldSumOfMask
+		print "newSumOfMask = %d" % newSumOfMask
+		if oldSumOfMask-newSumOfMask < n and newSumOfMask < (n**2):
 			print 'We have probably covered all the voxels of interest.'
 			moreToProcess = False
+		oldSumOfMask = newSumOfMask
+
+		if M >= Mmax:
+			print 'We have reached Mmax = %.2f.' % Mmax
+			moreToProcess = False		
 
 		# Update query resolution
 		M += Mstep
 
 
-	# Set all voxels that were outside of the mask or that failed all resolution tests to 10*M_final
+	# Set all voxels that were outside of the mask or that failed all resolution tests to M+Mstep
 	zeroVoxels = (resTOTAL==0)
 	resTOTAL[zeroVoxels] = M+Mstep
 
@@ -319,21 +434,24 @@ if __name__ == '__main__':
 	# Plots
 	resTOTALma  = np.ma.masked_where(resTOTAL == M+Mstep, resTOTAL)
 
-	f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-	ax1.imshow(data[:,:,int(3*n/9)], cmap=plt.cm.gray, interpolation="nearest")
-	ax2.imshow(data[:,:,int(4*n/9)], cmap=plt.cm.gray, interpolation="nearest")
-	ax3.imshow(data[:,:,int(5*n/9)], cmap=plt.cm.gray, interpolation="nearest")
-	ax4.imshow(data[:,:,int(6*n/9)], cmap=plt.cm.gray, interpolation="nearest")
+	print "\nMEAN RESOLUTION = %.2f" % np.ma.mean(resTOTALma)
 
+	f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+	ax1.imshow(data[int(3*n/9),:,:], cmap=plt.cm.gray, interpolation="nearest")
+	ax2.imshow(data[int(4*n/9),:,:], cmap=plt.cm.gray, interpolation="nearest")
+	ax3.imshow(data[int(5*n/9),:,:], cmap=plt.cm.gray, interpolation="nearest")
+	ax4.imshow(data[int(6*n/9),:,:], cmap=plt.cm.gray, interpolation="nearest")
+ 
 	f2, ((ax21, ax22), (ax23, ax24)) = plt.subplots(2, 2)
-	ax21.imshow(data[:,:,int(3*n/9)], cmap=plt.cm.gray, interpolation="nearest")
-	ax22.imshow(data[:,:,int(4*n/9)], cmap=plt.cm.gray, interpolation="nearest")
-	ax23.imshow(data[:,:,int(5*n/9)], cmap=plt.cm.gray, interpolation="nearest")
-	ax24.imshow(data[:,:,int(6*n/9)], cmap=plt.cm.gray, interpolation="nearest")
-	ax21.imshow(resTOTALma[:,:,int(3*n/9)], cmap=plt.cm.jet, interpolation="nearest", alpha=0.25)
-	ax22.imshow(resTOTALma[:,:,int(4*n/9)], cmap=plt.cm.jet, interpolation="nearest", alpha=0.25)
-	ax23.imshow(resTOTALma[:,:,int(5*n/9)], cmap=plt.cm.jet, interpolation="nearest", alpha=0.25)
-	ax24.imshow(resTOTALma[:,:,int(6*n/9)], cmap=plt.cm.jet, interpolation="nearest", alpha=0.25)
+	ax21.imshow(data[int(3*n/9),:,:], cmap=plt.cm.gray, interpolation="nearest")
+	ax22.imshow(data[int(4*n/9),:,:], cmap=plt.cm.gray, interpolation="nearest")
+	ax23.imshow(data[int(5*n/9),:,:], cmap=plt.cm.gray, interpolation="nearest")
+	ax24.imshow(data[int(6*n/9),:,:], cmap=plt.cm.gray, interpolation="nearest")
+	cax = ax21.imshow(resTOTALma[int(3*n/9),:,:], cmap=plt.cm.jet, interpolation="nearest", alpha=0.25)
+	ax22.imshow(resTOTALma[int(4*n/9),:,:], cmap=plt.cm.jet, interpolation="nearest", alpha=0.25)
+	ax23.imshow(resTOTALma[int(5*n/9),:,:], cmap=plt.cm.jet, interpolation="nearest", alpha=0.25)
+	ax24.imshow(resTOTALma[int(6*n/9),:,:], cmap=plt.cm.jet, interpolation="nearest", alpha=0.25)
+	cbar = f2.colorbar(cax)
 	plt.show()
 
 
